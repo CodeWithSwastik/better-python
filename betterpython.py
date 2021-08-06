78f24afebbce734bce7b9a3be5c48e440fa9f6d2
unknown <codewithswastik@gmail.com>
2021-08-06 19:14:09 +0530

    Add list sub

--- betterpython.py
+++ betterpython.py
@@ -9,10 +9,39 @@ def modify(*objects):
 
     return decorator
 
-@modify(str, list, tuple)
+def unsupported_type(op, obj1, obj2):
+    return TypeError(f"unsupported operand type(s) for {op}: '{obj1.__class__.__name__}' and '{obj2.__class__.__name__}'")
+
+@modify(str)
 def __invert__(self):
-    return self[::-1]
+    return ''.join(reversed(self))
+
+@modify(list, tuple)
+def __invert__(self):
+    return self.__class__(reversed(self))
 
 @modify(dict)
 def __invert__(self):
     return {v:k for k,v in self.items()}
+
+@modify(str)
+def __sub__(self, other:str):
+    if isinstance(other, str):
+        return self.replace(other,'')
+    else:
+        raise unsupported_type('-', self, other)
+
+
+@modify(list, tuple)
+def __sub__(self, other):
+    if isinstance(other, (list, tuple, set)):
+        return self.__class__(elem for elem in self if elem not in other)
+    else:
+        raise unsupported_type('-', self, other)
+
+# Doesn't work :/
+# @modify(str)
+# def __setitem__(self, key, value):
+#     temp = list(self)
+#     temp[key] = str(value)
+#     self = ''.join(temp)
--- tests.py
+++ tests.py
@@ -4,3 +4,5 @@ assert ~'Yes' == 'seY'
 assert ~[1,2,3] == [3,2,1]
 assert ~('x','y') == ('y','x')
 assert ~{0:1} == {1:0}
+assert 'test' - 't' == 'es'
+assert [1,2,3,1,4,3] - (1,3) == [2,4]
